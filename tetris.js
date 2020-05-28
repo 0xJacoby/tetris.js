d9f250b75a37bc0d861349fdb7802b68b8652879
Jacoby <jacoby34.dev@gmail.com>
2020-05-28 18:37:35 +0200

    added bugfix for edges and alternative controls

--- tetris.js
+++ tetris.js
@@ -3,31 +3,33 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 let gameGrid = Array(10).fill().map(a => Array(21).fill().map(a => -1)); // 10x21 block grid for pieces
-let tetro = {x: 0, y: 0, blocks: Array(4), shapeIndex: 1}; // the current position of the moving piece and its blocks.
+let tetro = {x: 0, y: 0, blocks: Array(4), shapeIndex: 1, rotation: 0}; // the current position of the moving piece and its blocks.
+let canMove = true;
+let canRotate = true;
 
 const blockSize = 40; // size of one block
 
 // All the different tetris shapes and their rotations. Thoroughly explained in the documentation.
 let tetrominos = {
-    i: { rotations: [0x0F00, 0x2222, 0x00F0, 0x4444], img: "url()", color: "cyan" },
+    i: { rotations: [0x0F00, 0x2222, 0x0F00, 0x2222], img: "url()", color: "cyan" },
     j: { rotations: [0x44C0, 0x8E00, 0x6440, 0x0E20], img: "url()", color: "blue" },
     l: { rotations: [0x4460, 0x0E80, 0xC440, 0x2E00], img: "url()", color: "orange" },
     o: { rotations: [0xCC00, 0xCC00, 0xCC00, 0xCC00], img: "url()", color: "yellow" },
-    s: { rotations: [0x06C0, 0x8C40, 0x6C00, 0x4620], img: "url()", color: "green" },
+    s: { rotations: [0x06C0, 0x8C40, 0x06C0, 0x8C40], img: "url()", color: "green" },
     t: { rotations: [0x0E40, 0x4C40, 0x4E00, 0x4640], img: "url()", color: "purple" },
-    z: { rotations: [0x0C60, 0x4C80, 0xC600, 0x2640], img: "url()", color: "red" }
+    z: { rotations: [0x0C60, 0x4C80, 0x0C60, 0x4C80], img: "url()", color: "red" }
 };
 
-function drawTetromino(rotation) {
+function drawTetromino() {
     let row = 0, column = 0;
     let shape = Object.values(tetrominos)[tetro.shapeIndex];
-    let block = shape.rotations[rotation];
+    let block = shape.rotations[tetro.rotation];
     ctx.fillStyle = shape.color;
 
     for (let bit = 0x8000; bit > 0; bit = bit >> 1) {
         if (block & bit) {
-            tetro.blocks.push([tetro.x + column, tetro.y + row]);
             gameGrid[tetro.x + column][tetro.y + row] = tetro.shapeIndex;
+            tetro.blocks.push([tetro.x + column, tetro.y + row]);
             updateCanvas();
         }
         
@@ -68,6 +70,53 @@ function moveTetromino(distance) {
     }
 }
 
+function rotateTetromino() {
+    let temp = [];
+    let bugfix = [0x44C0, 0x2222, 0x6C0, 0x4C40, 0x4C80];
+    let block = Object.values(tetrominos)[tetro.shapeIndex].rotations[tetro.rotation];
+    let possible = true;
+
+    temp.push(tetro.rotation);
+    temp.push(tetro.blocks);
+    temp.push(gameGrid);
+
+    if (bugfix.indexOf(block) > -1) {
+        tetro.blocks.forEach(e => {
+            if (e[0] === 9) {
+                possible = false;
+            }
+        });
+    }
+
+    if (canRotate && possible) {
+        canRotate = false;
+
+        if (tetro.rotation === 3) {
+            tetro.rotation = 0;
+        } else {
+            tetro.rotation++;
+        }
+
+        tetro.blocks.forEach(e => {
+            gameGrid[e[0]][e[1]] = -1;
+        });
+
+        tetro.blocks = Array(4);
+        
+        try { 
+            drawTetromino(); 
+            temp = [];
+        } catch {
+            tetro.rotation = temp[0];
+            tetro.blocks = temp[1];
+            gameGrid = temp[2];
+        }
+
+        setTimeout(function(){ canRotate = true; }, 150);
+
+    }
+}
+
 // main canvas setup function
 function setupCanvas() {
     updateCanvas();
@@ -93,17 +142,56 @@ function updateCanvas() {
 document.querySelector('body').onkeydown = function(e) {
     switch (e.keyCode) {
         case 37: // left arrow key
-            moveTetromino(-1);
+            if (canMove) {
+                moveTetromino(-1);
+                canMove = false;
+                setTimeout(function(){ canMove = true; }, 50);
+            }
             break;
         case 38: // up arrow key
-
+            rotateTetromino();
             break;
         case 39: // right arrow key
-            moveTetromino(1);
+            if (canMove) {
+                moveTetromino(1);
+                canMove = false;
+                setTimeout(function(){ canMove = true; }, 50);
+            }
             break;
 
         case 40: // down arrow key
 
+            break;
+        
+
+        // alternative controls
+
+        case 32: // space bar key
+            rotateTetromino();
+            break;
+
+        case 87: // W key
+            rotateTetromino();
+            break;
+
+        case 65: // A key
+            if (canMove) {
+                moveTetromino(-1);
+                canMove = false;
+                setTimeout(function(){ canMove = true; }, 50);
+            }
+            break;
+
+        case 83: // S key
+            
+            break;
+
+        case 68: // D key
+            if (canMove) {
+                moveTetromino(1);
+                canMove = false;
+                setTimeout(function(){ canMove = true; }, 50);
+            }
             break;
     }
 };
